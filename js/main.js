a465c22103218f1f6e97350db060957166f76c1a
lucianasm86 <itsuptocode@gmail.com>
2018-05-06 16:39:59 -0700

    final exercise

--- js/main.js
+++ js/main.js
@@ -13,3 +13,48 @@ var myArray = ["this", "is", "a", "string", "array"];
 for (i = 0; i < myArray.length; i++) {
 	console.log(myArray[i]);
 }
+
+var myObject = {
+	name: "Luciana",
+	age: 32,
+	country: "Brazil"
+};
+
+console.log(myObject.name);
+console.log(myObject.age);
+console.log(myObject.country);
+
+
+var heading1 = document.getElementById("heading1");
+var heading2 = document.getElementById("heading2");
+var heading3 = document.getElementById("heading3");
+
+
+var array = [ 
+	{
+		city: "San Francisco",
+		state: "California",
+		country: "USA"
+	},
+	{
+		city: "Recife",
+		state: "Pernambuco",
+		country: "Brazil"
+	},
+	{
+		city: "Miami Beach",
+		state: "Florida",
+		country: "USA"
+	}
+];
+
+for (i = 0; i < array.length; i++) {
+	console.log(array[i].city);
+	console.log(array[i].state);
+	console.log(array[i].country);
+}
+
+document.body.style.color = "red";
+
+
+

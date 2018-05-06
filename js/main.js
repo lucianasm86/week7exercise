2f106cb69505a4a540f75c7f77a44c1f527dc955
lucianasm86 <itsuptocode@gmail.com>
2018-05-06 16:06:01 -0700

    exercise update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+var myH1 = document.getElementById("myH1");
+	myH1.innerText = "Tudo bem?";
+var myNewH1 = document.getElementById("myNewH1");
 
+function getInfo() {
+	var userInput = document.getElementById("userInput").value;
+	console.log(userInput);
+		myNewH1.innerText = userInput;
+}
+
+var myArray = ["this", "is", "a", "string", "array"];
+
+for (i = 0; i < myArray.length; i++) {
+	console.log(myArray[i]);
+}
